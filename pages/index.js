817a73c010258ae73ad7aaeec101313afaae7206
Nicholas Nguyen <nicholas.nguyen@shaw.ca>
2022-11-13 15:41:21 -0700

    Changes to the index & h1 files

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
+import h1 from "../components/h1";
+import Tagline from "../components/tagline";
 
 export default function Home() {
   return (
-    <body>
-    </body>
+    <main>
+
+    </main>
 
 
 
