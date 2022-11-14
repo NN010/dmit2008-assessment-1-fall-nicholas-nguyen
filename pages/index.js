382df6942a7917cad2c753230f78c16c07549214
Nicholas Nguyen <nicholas.nguyen@shaw.ca>
2022-11-13 18:37:07 -0700

    new commit

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
 import h1 from "../components/h1";
 import Tagline from "../components/tagline";
-import devsData from "../data/devs.js";
+import devs from "../data/devs.js";
+
+function h1();
+function Tagline();
 
 export default function Home() {
   return (
     <main>
-
+      <div>
+        {devs};
+      </div>
     </main>
 
 
