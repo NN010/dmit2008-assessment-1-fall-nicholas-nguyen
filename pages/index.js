b09f3cc0b4b5736387fcd0403fd227c0f7f81e01
Nicholas Nguyen <nicholas.nguyen@shaw.ca>
2022-12-16 11:53:36 -0700

    formatting changes

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import react from "react";
 import h1 from "../components/h1";
 import Tagline from "../components/tagline";
+import Navigation from "../components/navigation"
 import devs from "../data/devs.json";
 
-function h1();
-function Tagline();
+function h1()
+function Tagline()
 
 export default function Home() {
   return (
