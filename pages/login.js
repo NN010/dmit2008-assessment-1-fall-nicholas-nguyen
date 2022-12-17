871ce135683bd74170d06f135f2186ed82c4a257
Nicholas Nguyen <nicholas.nguyen@shaw.ca>
2022-12-16 20:46:24 -0700

    minor

--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,5 @@
 import react from "react";
+import next from "next";
 import h1 from "../components/h1";
 import Tagline from "../components/tagline";
 import Navigation from "../components/navigation";
@@ -10,7 +11,12 @@ export default function Login() {
             <h2>Login</h2>
             <form>
                 <div>
-
+                    <label for="email">Email Address</label>
+                    <input type="text" id="email" name="email" />
+                </div>
+                <div>
+                    <label for="passwd">Password</label>
+                    <input type="password" id="passwd" name="passwd" />
                 </div>
             </form>
 
