018b24de5fae928a1376ce6d6a47ff4d904268e7
Nicholas Nguyen <nicholas.nguyen@shaw.ca>
2022-12-16 18:22:32 -0700

    Error fixing

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import react from "react";
 import h1 from "../components/h1";
 import Tagline from "../components/tagline";
-import Navigation from "../components/navigation"
+import Navigation from "../components/navigation";
 import devs from "../data/devs.json";
 
 export default function Home() {
--- pages/profile.js
+++ pages/profile.js
@@ -1,6 +1,7 @@
 import react from "react";
 import h1 from "../components/h1";
 import Tagline from "../components/tagline";
+import Navigation from "../components/navigation";
 import devs from "../data/devs.json";
 
 <div>
