d56e2add1899e9c73f6a04fe8584944ff68d241c
Nicholas Nguyen <nicholas.nguyen@shaw.ca>
2022-12-16 23:03:52 -0700

    Maybe Last Commit?

--- pages/profile.js
+++ pages/profile.js
@@ -3,11 +3,17 @@ import h1 from "../components/h1";
 import Tagline from "../components/tagline";
 import Navigation from "../components/navigation";
 import devs from "../data/devs.json";
+import getStaticPaths from "index";
 
 export default function Profile() {
     return (
-        <main class="container">
-            {devs}
-        </main>
+        <div>
+            <p>{devs.name}</p>
+            <p>{devs.email}</p>
+            <p>{devs.Available_For_Work}</p>
+            <p>{devs.Phone}</p>
+            <p>{devs.Experience}</p>
+        </div>
     )
 }
+
